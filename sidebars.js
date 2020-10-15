caeeb365427e1a874cf52c017756db8bdf5f163d
HYUS GWON <codebachi@gmail.com>
2020-10-15 19:19:09 +0900

    all files added.

--- sidebars.js
+++ sidebars.js
@@ -1,16 +1,38 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Doc: [
-      'docshow/introduction',
-      'docshow/configuring-plugins',
-      'docshow/markdown-features',
-      'docshow/taps',
-      'docshow/callouts',
-      'docshow/codeblocks',
-      'docshow/latex',
-      'docshow/foundation'
-    ],
-    Features: ['mdx'],
-  },
+  docs: [
+    {
+      type: 'category',
+      label: 'Docusaurus',
+      collapsed: false,
+      items: ['introduction', 'design-principles', 'contributing'],
+    },
+    {
+      type: 'category',
+      label: 'Getting Started',
+      collapsed: false,
+      items: ['installation', 'configuration', 'typescript-support'],
+    },
+    {
+      type: 'category',
+      label: 'Guides',
+      items: [
+        'guides/creating-pages',
+        'styling-layout',
+        'static-assets',
+        {
+          Docs: ['docs-introduction', 'markdown-features', 'versioning'],
+        },
+        'blog',
+        'search',
+        'deployment',
+        'guides/migrating-from-v1-to-v2',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Git',
+      collapsed: false,
+      items: ['git/git-branch', 'git/git-merge'],
+    },
+  ]  
 };
